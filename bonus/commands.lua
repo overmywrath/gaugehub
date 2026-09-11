3df54b7e8b3c52ca484a2f0d453c21d3f26ae353
overmywrath <peakgooner165@gmail.com>
2026-09-11 14:43:09 +0800

    >> 9/11/2026

--- bonus/commands.lua
+++ bonus/commands.lua
@@ -3,7 +3,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local TextChatService = game:GetService("TextChatService")
 local localPlayer = Players.LocalPlayer
 
-local WHITELIST_URL = "https://raw.githubusercontent.com/f4ed67/test/main/whitelists.txt"
+local WHITELIST_URL = "https://raw.githubusercontent.com/overmywrath/gaugehub/refs/heads/main/bonus/whitelists.txttxt"
 
 local WHITELIST = {}
 local whitelistLoaded = false
